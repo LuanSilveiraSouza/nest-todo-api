ef068642ccd3c28d09e9457cf0367b6731e79a28
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2021-05-04 21:01:33 -0300

    feat: add health check endpoint

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,8 +17,8 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/health')
       .expect(200)
-      .expect('Hello World!');
+      .expect({ msg: `System's up` });
   });
 });
